083e33a5e302b74ee33035db7faa3e91300e3d47
Jake Lu <jylu1205@outlook.com>
2024-01-31 15:01:00 +0000

    Add neural network initialization and training code

--- main.py
+++ main.py
@@ -5,9 +5,28 @@ def generate_data(data_low: int, data_upper: int, range_lower_bound: int, range_
     output = []
     for i in range(data_size):
         len = np.random.randint(range_lower_bound, range_upper_bound)
-        unsort = np.random.randint(data_low, data_upper, len)
+        unsort = np.random.randint(data_low, data_upper, len)[:,np.newaxis]
         sorted = np.sort(unsort)
-        output.append(np.asarray([unsort, sorted]))
+        output.append((unsort, sorted))
     return output
 
-print(generate_data(-100, 100, 10, 20, 10))
+data = generate_data(-100, 100, 10, 20, 1000)
+
+
+weights = []
+network = []
+
+def init_weights(layer_config: list):
+    for i in range(len(layer_config)-1):
+        weights.append(np.random.randn(layer_config[i], layer_config[i + 1]))
+    for i in range(len(layer_config)):
+        network.append(np.zeros(layer_config[i]))
+
+init_weights([1,4,4,1])
+for i in weights:
+    print(i.shape)
+    
+for epoch, (train,test) in enumerate(data):
+    network[0] = train
+    for ind, val in enumerate(weights):
+        network[ind + 1] = np.tanh(np.dot(network[ind], val))
